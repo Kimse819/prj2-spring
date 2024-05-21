2345695326f9c1e87134553c8d5c225cb94ec4a7
sehyeon <gs1884@naver.com>
2024-05-21 12:14:07 +0900

    mapper, service 작성

--- src/main/java/com/prj2_spring/controller/board/BoardController.java
+++ src/main/java/com/prj2_spring/controller/board/BoardController.java
@@ -1,6 +1,8 @@
 package com.prj2_spring.controller.board;
 
+import com.prj2_spring.controller.service.board.BoardService;
 import com.prj2_spring.domain.board.Board;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/board")
+@RequiredArgsConstructor
 public class BoardController {
+
+    private final BoardService service;
 
     @PostMapping("add")
     public void add(@RequestBody Board board) {
-        System.out.println("board: " + board);
+
+        service.add(board);
     }
 }
