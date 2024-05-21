0ab81b246836622a1ee5cfb6e05a81cae05e104d
sehyeon <gs1884@naver.com>
2024-05-21 12:13:57 +0900

    mapper, service 작성

---
+++ src/main/java/com/prj2_spring/mapper/board/BoardMapper.java
@@ -0,0 +1,15 @@
+package com.prj2_spring.mapper.board;
+
+import com.prj2_spring.domain.board.Board;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface BoardMapper {
+
+    @Insert("""
+            INSERT INTO board (title, content, writer)
+            VALUES (#{title}, #{content}, #{writer})
+            """)
+    public int insert(Board board);
+}
